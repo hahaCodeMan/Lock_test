77debf505b58c995c3ff794de100c51e5ebb6eba
hahacodeman <909820361@qq.com>
2017-10-10 10:51:39 +0800

    Initial Commit

---
+++ Lock_testTests/Lock_testTests.m
@@ -0,0 +1,39 @@
+//
+//  Lock_testTests.m
+//  Lock_testTests
+//
+//  Created by Risen on 2017/10/10.
+//  Copyright © 2017年 Risen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lock_testTests : XCTestCase
+
+@end
+
+@implementation Lock_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
